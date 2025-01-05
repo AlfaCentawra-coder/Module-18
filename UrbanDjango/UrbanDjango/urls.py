4efb2d6fb561d7953c81e5f73725b7b5f00beac3
AlfaCentawra-coder <vovaluzin55@gmail.com>
2025-01-05 19:21:35 +0500

    fifth

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -14,15 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path
-from task4.views import pf, ct, gms
 
+from task5.views import sign_up_by_html, sign_up_by_django
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('platform/', pf.as_view()),
-    path('platform/games/', gms),
-    path('platform/cart/', ct.as_view()),
+    path('html_sign_up/', sign_up_by_html),
+    path('django_sign_up/', sign_up_by_django),
 ]
+# path('django_sign_up/', sign_up_by_django),
